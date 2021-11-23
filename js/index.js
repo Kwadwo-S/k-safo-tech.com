4c5b398238cdbe887b7208b3d4005c5aba2384d7
Kwadwo-S <83795932+Kwadwo-S@users.noreply.github.com>
2021-11-23 19:15:47 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -58,13 +58,13 @@ links.map(link => {
 
 
 
-// gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
-// gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
-// gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
-// gsap.from(".welcome", { opacity: 0, duration: 1, delay: 2.5, y: -50 });
-// gsap.from(".next", { opacity: 0, duration: 1, delay: 3, y: -45 });
-// gsap.from(".responsive", { opacity: 0, duration: 1, delay: 3.5, y: 50 }); 
-// gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 4.5, y: 55 }); 
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".welcome", { opacity: 0, duration: 1, delay: 2.5, y: -50 });
+gsap.from(".next", { opacity: 0, duration: 1, delay: 3, y: -45 });
+gsap.from(".responsive", { opacity: 0, duration: 1, delay: 3.5, y: 50 }); 
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 4.5, y: 55 }); 
 
 
 
@@ -90,3 +90,4 @@ links.map(link => {
 //     pinSpacing: false
 //   });
 // });
+
